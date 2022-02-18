c563fd18356bea077c331f3be3aebca4c02cfa58
SandeMiriam <miriamsandeoyuga@gmail.com>
2022-02-18 21:44:04 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,84 @@
+fun main() {
+    add()
+    division()
+    add2()
+    interestingfact()
+
+}
+fun add(){
+    var name ="miriam"
+    println("hello "+name)
+
+
+}
+
+fun division(){
+    var a=19
+    var b=6
+    var modulus = a%b
+    println(modulus)
+}
+fun add2(){
+    var a=5
+    var b=2
+    var c=1
+    var sum=a+b+c
+    println(sum)
+}
+fun interestingfact () {
+    var fact= "I love listening to music"
+    println(fact)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
